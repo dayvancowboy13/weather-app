c08335b514f3fc6de9173f26e1cf883b410113ad
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-07-12 10:19:47 -0700

    weatherapi object created to handle requests; working on JSON parser class

---
+++ main.js
@@ -0,0 +1,19 @@
+(self["webpackChunkweather_app"] = self["webpackChunkweather_app"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+alert('and so it begins');
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoJ2FuZCBzbyBpdCBiZWdpbnMnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
